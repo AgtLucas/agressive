0e6ee1611bedbeec5445227391b75591629fb37a
Lucas <lucas@agtlucas.com>
2015-08-25 16:47:06 -0300

    Update app.js

--- components/app.js
+++ components/app.js
@@ -1,21 +1,8 @@
 import React from 'react';
+import {RouteHandler} from 'react-router';
 
 export default class App extends React.Component {
-  constructor() {
-    super();
-    this.state = { n: 0 }
-  }
-
-  handleClick() {
-    this.setState({ n: this.state.n + 1 });
-  }
-
   render() {
-    return(
-      <div>
-        <h1>Clicked {this.state.n} times.</h1>
-        <button onClick={this.handleClick.bind(this)}>Click me!</button>
-      </div>
-    );
+    return <RouteHandler />
   }
 }
