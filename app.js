309ce7cd7582ec3635e8549dfae945b0579a6bbc
Lucas <lucas@agtlucas.com>
2015-08-25 16:19:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import hbs from 'express-handlebars';
 import React from 'react/addons';
+import serveStatic from 'serve-static';
 import App from './components/app';
 
 var app = express();
 app.engine('html', hbs({ extname: 'html' }));
 app.set('view engine', 'html');
 app.locals.settings['x-powered-by'] = false;
+app.use(serveStatic('public'));
 
 app.get('/', function home(req, res, next) {
   res.render('layout', {
