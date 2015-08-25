3c55be0729b113e79ea89b8ecf93773434781484
Lucas <lucas@agtlucas.com>
2015-08-25 15:31:48 -0300

    Update app.jsx

---
+++ components/app.jsx
@@ -0,0 +1,21 @@
+import React from 'React';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { n: 0 }
+  }
+
+  handleClick() {
+    this.setState({ n: this.state.n + 1 });
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Clicked {this.state.n} times.</h1>
+        <button onClick={this.handleClick.bind(this)}>Click me!</button>
+      </div>
+    );
+  }
+}
