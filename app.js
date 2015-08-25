e0b25c33a6537d10becfb3c7c7a124c13ffb9392
Lucas <lucas@agtlucas.com>
2015-08-25 13:36:24 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,3 +2,16 @@ import express from 'express';
 import hbs from 'express-handlebars';
 import React from 'react/addons';
 import App from './components/app';
+
+var app = express();
+app.engine('html', hbs({ extname: 'html' }));
+app.set('view engine', 'html');
+app.locals.settings['x-powered-by'] = false;
+
+app.get('/', function home(req, res, next) {
+  res.render('layout', {
+    reactHtml: React.renderToString(<App />);
+  });
+});
+
+app.listen(process.env.PORT || 3000);
