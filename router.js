6afc1f7f4db7ab6fecdb519a812bae9af8d54c13
Lucas <lucas@agtlucas.com>
2015-08-26 10:27:44 -0300

    Update router.js

---
+++ router.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Router from 'react-router';
+import routes from './routes';
+
+export default class function router(req, res, next) {
+  var context = {
+    routes: routes,
+    location: req.url
+  };
+  Router.create(context).run(function ran(Handler, state) {
+    reactHtml: React.renderToString(<Handler />)
+  });
+}
