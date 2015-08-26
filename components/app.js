88b789d04c5d2bbe8178e392bc26b78c8e96b738
Lucas <lucas@agtlucas.com>
2015-08-26 10:47:42 -0300

    Update app.js

--- components/app.js
+++ components/app.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import {RouteHandler} from 'react-router';
+import DocumentTitle from 'react-document-title';
 
 export default class App extends React.Component {
   render() {
-    return <RouteHandler />
+    return(
+      <DocumentTitle title="Yoo!">
+        <RouteHandler />
+      </DocumentTitle>
+    )
   }
 };
