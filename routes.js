58969e6932b5ee94eaa67f91b2a90c7ba6e0b13b
Lucas <AgtLucas@users.noreply.github.com>
2016-01-11 09:10:35 -0200

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import {Route, DefaultRoute} from 'react-router';
-import App from './components/app';
-import HomeIndex from './components/home/index';
+import React from 'react'
+import {Route, DefaultRoute} from 'react-router'
+import App from './components/app'
+import HomeIndex from './components/home/index'
 
 export default (
   <Route path='/' handler={App}>
     <DefaultRoute handler={HomeIndex} />
   </Route>
-);
+)
