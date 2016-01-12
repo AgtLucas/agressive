7fba1ee67ec9684126e7c8b7618a36e2e65284bd
Lucas <AgtLucas@users.noreply.github.com>
2016-01-12 04:13:57 -0200

    Update main.js

--- client/main.js
+++ client/main.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import Router from 'react-router';
-import routes from '../routes';
+import React from 'react'
+import Router from 'react-router'
+import routes from '../routes'
 
-var main = document.getElementsByTagName('main')[0];
+var main = document.getElementsByTagName('main')[0]
 
 Router.run(routes, Router.HistoryLocation, function ran(Handler, state) {
-  React.render(<Handler />, main);
-});
+  React.render(<Handler />, main)
+})
