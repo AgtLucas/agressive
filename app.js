aa0eb0dc4fa1dbd0326f7b49d37461a196f7082f
Lucas <AgtLucas@users.noreply.github.com>
2016-01-17 19:38:57 -0200

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import hbs from 'express-handlebars';
-import React from 'react/addons';
-import serveStatic from 'serve-static';
-import Router from 'react-router';
-import App from './components/app';
-import router from './router';
+import express from 'express'
+import hbs from 'express-handlebars'
+import React from 'react/addons'
+import serveStatic from 'serve-static'
+import Router from 'react-router'
+import App from './components/app'
+import router from './router'
 
-var app = express();
-app.engine('html', hbs({ extname: 'html' }));
-app.set('view engine', 'html');
-app.locals.settings['x-powered-by'] = false;
-app.use(serveStatic('public'));
-app.use(router);
+var app = express()
+app.engine('html', hbs({ extname: 'html' }))
+app.set('view engine', 'html')
+app.locals.settings['x-powered-by'] = false
+app.use(serveStatic('public'))
+app.use(router)
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000)
