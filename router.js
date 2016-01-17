a525550301bb9020e0229ee3be34be046c37482f
Lucas <AgtLucas@users.noreply.github.com>
2016-01-17 19:39:15 -0200

    Update router.js

--- router.js
+++ router.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import Router from 'react-router';
-import Layout from './components/Layout';
-import routes from './routes';
+import React from 'react'
+import Router from 'react-router'
+import Layout from './components/Layout'
+import routes from './routes'
 
 export default function router(req, res, next) {
   var context = {
     routes: routes,
     location: req.url
-  };
-  Router.create(context).run(ran);
-  function ran(Handler, state) {
-    var doctype = '<!doctype html>';
-    var main = React.renderToString(<Handler />);
-    var full = React.renderToString(<Layout main={main} />);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(doctype + full);
-    res.end();
   }
-};
+  Router.create(context).run(ran)
+  function ran(Handler, state) {
+    var doctype = '<!doctype html>'
+    var main = React.renderToString(<Handler />)
+    var full = React.renderToString(<Layout main={main} />)
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(doctype + full)
+    res.end()
+  }
+}
